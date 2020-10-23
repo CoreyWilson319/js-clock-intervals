f339165e47cc8f05059f08bf106577332b9325da
Your Name <you@example.com>
2020-10-23 12:04:10 -0400

    [clock.js] added seconds minutes and hours rotation function

---
+++ clock.js
@@ -0,0 +1,13 @@
+function secondRotation(hand) {
+    return  hand * 6
+}
+
+function minuteRotation(hand) {
+    return  hand * 0.1
+}
+function hourRotation(hand) {
+    return  hand * (0.1/60)
+}
+console.log(secondRotation(2))
+console.log(minuteRotation(1))
+console.log(hourRotation(1))
